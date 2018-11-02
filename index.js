34ebe5179e125adebeead289980bd200885b5b29
Michael Probber <michael@probber.com>
2018-11-01 17:19:13 -0700

    Fully flowtype, start out orchestra

--- index.js
+++ index.js
@@ -15,14 +15,6 @@ module.exports = ({ entry, name, template, favicon }) => (_, argv) => ({
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: [
-              ['@babel/plugin-proposal-decorators', { legacy: true }],
-              ['@babel/plugin-proposal-class-properties', { loose: true }],
-              '@babel/plugin-transform-flow-strip-types',
-            ],
-          },
         },
       },
       {
